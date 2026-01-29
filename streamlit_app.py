b8aeb8b16000fd5fbf7c86c193d112fcd7c75485
Udita Agarwal <89858880+uditaagarwal31@users.noreply.github.com>
2026-01-29 17:05:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,10 @@ if ingredients_list:
  
     #st.write(ingredients_string)
 
-    #my_insert_stmt = """
-        #INSERT INTO SMOOTHIES.PUBLIC.ORDERS (INGREDIENTS, NAME_ON_ORDER)
-        #VALUES ('""" + ingredients_string + """','""" + name_on_order + """')
-        #"""
+    my_insert_stmt = """
+        INSERT INTO SMOOTHIES.PUBLIC.ORDERS (INGREDIENTS, NAME_ON_ORDER)
+        VALUES ('""" + ingredients_string + """','""" + name_on_order + """')
+        """
     #st.write(my_insert_stmt)
     
     time_to_insert = st.button('Submit Order')
